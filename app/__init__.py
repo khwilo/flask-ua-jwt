d1e6423691f74ec21b551cd5b79cb18dc7d27f81
khwilo <khwilowatai@gmail.com>
2019-04-23 18:24:20 +0300

    Read the app configuration values from the  file

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ from flask import Flask
 
 from app.api.v1 import AUTH_BLUEPRINT
 
+from instance.config import APP_CONFIG
 
-def create_app():
+
+def create_app(config_name):
     """
     Instantiate the Flask API
     """
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object(APP_CONFIG[config_name])
+    app.config.from_pyfile("config.py")
 
     app.register_blueprint(AUTH_BLUEPRINT)
 
