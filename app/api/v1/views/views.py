fab78656c25baac9acbb240f1cccb2f5e78359ab
khwilo <khwilowatai@gmail.com>
2019-04-24 21:51:40 +0300

    Implement the logic to register a user for an account

--- app/api/v1/views/views.py
+++ app/api/v1/views/views.py
@@ -1,17 +1,42 @@
 """Module for the user view"""
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+from app.api.v1.models.models import UserModel
 
 
 class UserRegistration(Resource):
     """
     User Registration Resource
     """
+    def post(self):
+        """Create a user account"""
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument(
+            "firstname",
+            type=str, required=True, help="firstname cannot be blank")
+        parser.add_argument(
+            "lastname",
+            type=str, required=True, help="lastname cannot be blank")
+        parser.add_argument(
+            "email",
+            type=str, required=True, help="email cannot be blank")
+        parser.add_argument(
+            "password",
+            type=str, required=True, help="password cannot be blank")
 
-    def get(self):
-        """
-        Welcome message
-        """
+        data = parser.parse_args()
+
+        # Create an instance of a user
+        user = UserModel(
+            firstname=data["firstname"],
+            lastname=data["lastname"],
+            email=data["email"],
+            password=data["password"]
+        )
+
+        user.save()
+
         return {
-            "status": 200,
-            "message": "Welcome to Flask UA"
-        }, 200
+            "status": 201,
+            "message": "User account was created successfully"
+        }, 201
