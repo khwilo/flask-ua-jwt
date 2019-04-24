52943faf72353f9ea9ec2c90f8f3619ffe2febb3
khwilo <khwilowatai@gmail.com>
2019-04-24 21:53:59 +0300

    Test if a user is able to create an account

--- app/tests/v1/test_user.py
+++ app/tests/v1/test_user.py
@@ -1,6 +1,7 @@
 """Test cases for the user authentication"""
 import json
 
+from app.tests.v1.sample_data import USER_REGISTRATION
 from app.tests.v1.test_base import BaseTestCase
 
 
@@ -8,11 +9,15 @@ class UserTestCase(BaseTestCase):
     """Test definitions for user authentication"""
     def test_user_registration(self):
         """Test a user can register for an account"""
-        res = self.client.get(
+        res = self.client.post(
             "/v1/auth/signup",
-            headers=BaseTestCase.get_accept_content_type_headers()
+            headers=BaseTestCase.get_accept_content_type_headers(),
+            data=json.dumps(USER_REGISTRATION)
         )
         response_msg = json.loads(res.data.decode("UTF-8"))
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 201)
         self.assertTrue(response_msg["message"])
-        self.assertEqual(response_msg["message"], "Welcome to Flask UA")
+        self.assertEqual(
+            response_msg["message"],
+            "User account was created successfully"
+        )
