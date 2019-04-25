c1795688618d2b0e84f8c6f508975a709d6c136c
khwilo <khwilowatai@gmail.com>
2019-04-25 11:58:19 +0300

    Implement the method to search a user from the 'users' table

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,6 +1,7 @@
 """Base model definitions"""
 from psycopg2.extras import RealDictCursor
 
+from app.api.v1.models.queries import find_user_by_value
 from manage import establish_connection
 
 
@@ -9,3 +10,10 @@ class BaseModel:
     def __init__(self):
         self.connection = establish_connection()
         self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
+
+    def search_user(self, column, value):
+        """Fetch a user from the 'users' table"""
+        query = find_user_by_value(column, value)
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result
