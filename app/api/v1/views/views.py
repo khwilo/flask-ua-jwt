ba150ed649bff6cca432f953efc53a57437020c6
khwilo <khwilowatai@gmail.com>
2019-04-29 18:04:46 +0300

    Implement the logic to get a user's info

--- app/api/v1/views/views.py
+++ app/api/v1/views/views.py
@@ -1,5 +1,5 @@
 """Module for the user view"""
-from flask import abort
+from flask import abort, request
 from flask_restful import Resource, reqparse
 
 from app.api.v1.models.models import UserModel
@@ -82,7 +82,42 @@ class UserLogin(Resource):
 
         auth_token = user.encode_auth_token(current_user["id"])
         return {
-            "status": 201,
+            "status": 200,
             "auth_token": auth_token.decode(),
             "message": "You have successfully logged in"
         }, 200
+
+
+class UserInfo(Resource):
+    """
+    User information resource
+    """
+    def get(self):
+        """Fetch a user's information"""
+        auth_header = request.headers.get("Authorization")
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                abort(401, {
+                    "error": "Bearer token is malformed"
+                })
+        else:
+            auth_token = " "
+        if auth_token:
+            response = UserModel.decode_auth_token(auth_token)
+            if not isinstance(response, str):
+                user = UserModel().search_user("id", response)
+                return {
+                    "status": 200,
+                    "user": UserModel.to_json(user)
+                }, 200
+            abort(401, {
+                "status": 401,
+                "error": response
+            })
+        else:
+            abort(401, {
+                "status": 401,
+                "error": "Please provide a valid authentication token"
+            })
